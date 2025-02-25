db95fd526572ffa6048ae1c28053a40699c11bcd
Cameron <105471409+keraion@users.noreply.github.com>
2025-02-25 07:58:16 -0500

    Move around python stuff

---
+++ rsqlfluff_poc/sample.py
@@ -0,0 +1,30 @@
+import time
+import glob
+
+from sqlfluff.core.dialects import dialect_readout
+from sqlfluff.core.parser.lexer import Lexer
+import rsqlfluff
+
+for dialect in dialect_readout():
+    print(dialect.label)
+    sql_all = ""
+    for f in glob.glob(f"../sqlfluff/test/fixtures/dialects/{dialect.label}/*.sql"):
+        if "obevo" in f:
+            continue
+        with open(f, "r", encoding="utf8") as fh:
+            sql_all += fh.read()
+
+    lexer = Lexer(dialect=dialect.label)
+    py_start = time.time()
+    py_out = lexer.lex(sql_all)
+    py_end = time.time()
+
+    rs_start = time.time()
+    rs_out = rsqlfluff.lex(sql_all, True, dialect.label)
+    rs_end = time.time()
+    for p, r in zip(py_out[0], rs_out[0]):
+        assert p.raw == r.raw
+    print(py_end - py_start)
+    print(rs_end - rs_start)
+    print(((py_end - py_start) - (rs_end - rs_start)) / (py_end - py_start))
+    print(f"{dialect.label} Speedup: {(py_end - py_start) / (rs_end - rs_start)}")
